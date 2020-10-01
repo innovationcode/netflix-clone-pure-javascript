375cb94166820452d23d0d19a9b4eef9a1cbb253
lajawanti <lajawanti.dhake@gmail.com>
2020-10-01 18:24:17 -0400

    Trending now movie fetched

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = () =>  {
       fetchMovies();
+      fetchTrendingMovies();
 }
   
 function fetchMovies(){
@@ -12,6 +13,7 @@ function fetchMovies(){
           }
       })
       .then((data)=>{
+          console.log("RESPONSE  ...  ", data)  
           addMovies(data);
       })
       .catch((error_data)=>{
@@ -30,3 +32,35 @@ function addMovies(movies){
           moviesEl.innerHTML += image;
       }
 }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function fetchTrendingMovies(){
+      fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045")
+      .then((response)=>{
+          if (response.ok) {
+              return response.json();
+          } else {
+              throw new Error("something went wrong");
+          }
+      })
+      .then((data)=>{
+          console.log("Trending Movie RESPONSE  ...  ", data)  
+          showTrendingMovies(data);
+      })
+      .catch((error_data)=>{
+          console.log(error_data);
+      })
+}
+  
+// Add movies to the front end
+function showTrendingMovies(movies){
+      // Add img element to original__movies element
+      var moviesEl = document.querySelector('#trending');
+      for(var movie of movies.results){
+          var image = `
+              <img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"></img>
+          `
+          moviesEl.innerHTML += image;
+      }
+}
+
