499c0149bee5b8038e5ca5b1f3944e6e4de4a796
lajawanti <lajawanti.dhake@gmail.com>
2020-10-01 19:19:01 -0400

    Top rated movie fetched

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = () =>  {
       fetchMovies();
       fetchTrendingMovies();
+      fetchTopRatedMovies();
 }
   
 function fetchMovies(){
@@ -33,7 +34,7 @@ function addMovies(movies){
       }
 }
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++  Trending Movies  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function fetchTrendingMovies(){
       fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045")
       .then((response)=>{
@@ -56,6 +57,38 @@ function fetchTrendingMovies(){
 function showTrendingMovies(movies){
       // Add img element to original__movies element
       var moviesEl = document.querySelector('#trending');
+      for(var movie of movies.results){
+          var image = `
+              <img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"></img>
+          `
+          moviesEl.innerHTML += image;
+      }
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++  Top Rated Movies  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function fetchTopRatedMovies(){
+      fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1")
+      .then((response)=>{
+          if (response.ok) {
+              return response.json();
+          } else {
+              throw new Error("something went wrong");
+          }
+      })
+      .then((data)=>{
+          console.log("Top rated Movie RESPONSE  ...  ", data)  
+          showTopRatedMovies(data);
+      })
+      .catch((error_data)=>{
+          console.log(error_data);
+      })
+}
+  
+// Add movies to the front end
+function showTopRatedMovies(movies){
+      // Add img element to original__movies element
+      var moviesEl = document.querySelector('#top-rated');
       for(var movie of movies.results){
           var image = `
               <img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"></img>
